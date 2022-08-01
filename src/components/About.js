613a52cb74105e5faaa4fca8cd4907d8bc9712a9
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-08-01 07:24:14 +0530

    adding new component

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ const About = () => {
       <Box display="flex" flexDirection="column" alignItems="center">
         <Typography sx={{fontFamily:'fantasy'}} variant="h2">This ia a CRUD Application</Typography>
         <Typography variant="h3">By MERN Stack</Typography>
+        
       </Box>
     </div>
   );
